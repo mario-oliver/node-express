62fe1df92b04fcb7af2ca8d7b77a9c554ce9bd5c
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-08 17:57:29 -0400

    continuation of previous commit

--- express_tutorial/app.js
+++ express_tutorial/app.js
@@ -3,19 +3,20 @@ const app = express();
 const path = require('path');
 
 //move resources of the navbar app into a new public folder
+//static is a set of files that are not changing/dynamic
 app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
   /**
-     * to get back a webpage we'll use the following:
-     * use path to get the path of the files of our website by using --> resolve()
-     * then we'll pass the pathfile by:
-     *           resolving the path with path.resolve()
-     *                  node global object __dirname to get our server's directory
-                        and now within the server, pointing to the root of our websites (index.html)
-     * One last thing is that we use sendFile() method not send
-     * FYI could use path.join() with the same reoslving of the path (params)
-     */
+   * to get back a webpage we'll use the following:
+   * use path to get the path of the files of our website by using --> resolve()
+   * then we'll pass the pathfile by:
+   *         - resolving the path with path.resolve()
+   *                  - node global object __dirname to get our server's directory
+   *                  - and now within the server, pointing to the root of our websites (index.html)
+   * One last thing is that we use sendFile() method not send
+   * FYI could use path.join() with the same reoslving of the path (params)
+   */
   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
 });
 
