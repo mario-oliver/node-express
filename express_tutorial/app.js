09af782bba6834bc01fb8f5af8f6bd8f06fb9d6f
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-19 19:15:57 -0400

    added delete method

--- express_tutorial/app.js
+++ express_tutorial/app.js
@@ -55,6 +55,17 @@ app.put('/api/people/:id', (req, res) => {
   }
 });
 
+app.delete('/api/people/:id', (req, res) => {
+  const { id } = req.params;
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    res.status(400).json({ success: false, msg: `no person with id ${id}` });
+  }
+  const newPeople = people.filter((person) => person.id !== Number(id));
+  return res.status(200).json({ success: true, data: newPeople });
+});
+
 app.listen(5000, () => {
   console.log('Server listening on 5000');
 });
