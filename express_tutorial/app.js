1cc95d16a291ef1332d6f050fb356c885ee00a79
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-19 18:37:17 -0400

    login functionality to work with html form action pointing to the /login express endpoint where we use the express.urlencoded middleware function for adding data to the request body. Then in the login method we access the name object that is encoded and show that to the user. Note that this action uses the index.html form and NOT the javascript.html

--- express_tutorial/app.js
+++ express_tutorial/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 let { people } = require('./data');
-const path = require('path');
 
 // static
 app.use(express.static('./methods-public'));
 
+//middleware to pase form data to put into the request response (otherwise we don't have access to form parameters)
+//this is only for index.html that uses form action and form input to encode the name object
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, people: people });
+});
+
+app.post('/login', (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  console.log(req.body);
+  res.status(401).send('Please Provide Credentials');
 });
 
 app.listen(5000, () => {
