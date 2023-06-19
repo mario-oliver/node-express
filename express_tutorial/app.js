175108fd0fe6e588463587d235ee8c8c101c1237
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-19 12:45:34 -0400

    //can supply a url extension to tell the use() function what urls to attach the middleware to

--- express_tutorial/app.js
+++ express_tutorial/app.js
@@ -1,12 +1,13 @@
 const app = require('express')();
 const logger = require('./logger');
 
+//order does matter - root method will not utilize our logger middleware function, but the ones below will
+//can supply a url extension to tell the use() function what urls to attach the middleware to
+app.use('/api', logger);
+
 app.get('/', (req, res) => {
   res.send('Home');
 });
-
-//order does matter - root method will not utilize our logger middleware function, but the ones below will
-app.use(logger);
 
 app.get('/about', (req, res) => {
   res.send('About');
