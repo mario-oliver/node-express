a27df424067146aa2de22db6083c639b46416c52
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-19 18:47:06 -0400

    Added a second flavor working with javascript code - plain vanilla js - that sets the content type and then shows the newly added name property that is passed into the body of a post() request. we then access that data through a query with axios library to our new api post endpoint

--- express_tutorial/app.js
+++ express_tutorial/app.js
@@ -9,17 +9,32 @@ app.use(express.static('./methods-public'));
 //this is only for index.html that uses form action and form input to encode the name object
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/api/people', (req, res) => {
-  res.status(200).json({ success: true, people: people });
-});
-
 app.post('/login', (req, res) => {
+  //uses express.urlencoded() middelware to access the req.body
+
   const { name } = req.body;
   if (name) {
     return res.status(200).send(`Welcome ${name}`);
   }
   console.log(req.body);
   res.status(401).send('Please Provide Credentials');
+});
+
+//for handling json functionality in the request
+//similar to urlencode express.json() gives us access to json data encoded in the http method
+app.use(express.json());
+
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, people: people });
+});
+
+app.post('/api/people', (req, res) => {
+  //uses express.json() middelware to access the req.body
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ success: false, msg: 'please provide name value' });
+  }
+  res.status(201).json({ success: true, person: name });
 });
 
 app.listen(5000, () => {
