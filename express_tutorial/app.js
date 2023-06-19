76cc0482c355b0bd9030317ac46bf389dbb108ec
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-19 13:09:50 -0400

    utilizing express middleware and third party middleware rather than our own

--- express_tutorial/app.js
+++ express_tutorial/app.js
@@ -1,11 +1,20 @@
 const app = require('express')();
 const logger = require('./logger');
 const authorize = require('./authorize');
+const morgan = require('morgan');
 
-//order does matter - root method will not utilize our logger middleware function, but the ones below will
-//can supply a url extension to tell the use() function what urls to attach the middleware to
-//the list of middleware is executed in order
-// app.use([logger, authorize]);
+// req => middleware => res
+
+// 1. invoke two ways == use vs route
+// 2. middleware options - 1) our own middleware OR 2) express's own middleware OR 3) third party middelware
+
+//an example of express's middleware  is the "static" middleware function used previously to require all resources that are static in our webpage -> simple-server-static-website.js example
+// app.use(express.static('./public'));
+
+//an example of third party middleware is morgan -> https://www.npmjs.com/package/morgan
+//reference docs for what tiny does
+//in short it gives you the smallest bit of needed token code
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.send('Home');
