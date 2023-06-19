2b8b3cc0175194efbb67964285b908504c3e3eb9
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-19 12:42:55 -0400

    moved middleware invokation into use() function - NOTE: order matters as method calls above the .use(logger) Mid.Func. will not utilize the function

--- express_tutorial/app.js
+++ express_tutorial/app.js
@@ -1,12 +1,23 @@
 const app = require('express')();
 const logger = require('./logger');
 
-app.get('/', logger, (req, res) => {
+app.get('/', (req, res) => {
   res.send('Home');
 });
 
+//order does matter - root method will not utilize our logger middleware function, but the ones below will
+app.use(logger);
+
 app.get('/about', (req, res) => {
   res.send('About');
+});
+
+app.get('/api/products', (req, res) => {
+  res.send('Home');
+});
+
+app.get('/api/items', (req, res) => {
+  res.send('Home');
 });
 
 app.listen(5000, () => {
