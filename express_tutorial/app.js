bf8e47ea70773eb1933d9b5a428d03fadba4419b
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-19 21:48:12 -0400

    cont

--- express_tutorial/app.js
+++ express_tutorial/app.js
@@ -10,14 +10,13 @@ app.use(express.static('./methods-public'));
 //this is only for index.html that uses form action and form input to encode the name object
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/login', auth);
-
 //for handling json functionality in the request
 //similar to urlencode express.json() gives us access to json data encoded in the http method
 app.use(express.json());
 
 //hre is the base url that will be use with the express router
 app.use('/api/people', people);
+app.use('/login', auth);
 
 app.listen(5000, () => {
   console.log('Server listening on 5000');
